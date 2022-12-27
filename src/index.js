7e595fd83ee92dcb95fbeadea347142e8950bc42
Gabriel Nuñez <gabriel.nzn@gmail.com>
2022-12-27 07:56:49 +0000

    added pause functionality

--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ const video = document.querySelector('video')
 const button = document.querySelector('button')
 
 function MediaPlayer(config) {
+    this.isPlaying = false
     this.media = config.mediaElement
 }
 
-MediaPlayer.prototype.play = function () {
-    this.media.play()
+MediaPlayer.prototype.playOrPause = function () {
+    if (this.isPlaying) {
+        this.isPlaying = !this.isPlaying
+        return this.media.pause()
+    }
+    this.isPlaying = !this.isPlaying
+    return this.media.play()
 }
 
 const player = new MediaPlayer({ mediaElement: video })
 
-button.onclick = () => player.play()
+button.onclick = () => player.playOrPause()
